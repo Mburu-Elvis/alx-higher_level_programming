d3bde656d960343dc071609b6e71d929af3f6122
root <mburu.elvis1@students.jkuat.ac.ke>
2022-12-05 18:33:20 +0000

    Str.format

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    item = 0
-    str = "{}"
-    while item < len(my_list):
-        print(str.format(my_list[item]))
-        item += 1
+    for i in my_list:
+        print("{:d}".format(i))
