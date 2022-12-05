a6631d8af2384f0edb8154069229dfabfc997713
root <mburu.elvis1@students.jkuat.ac.ke>
2022-12-05 18:29:13 +0000

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     item = 0
+    str = "{}"
     while item < len(my_list):
-        print("{}".format(my_list[item]))
+        print(str.format(my_list[item]))
         item += 1
