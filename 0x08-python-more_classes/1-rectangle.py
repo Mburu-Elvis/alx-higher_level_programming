144f6535a64d3d5eadf72d8c02a59edc1fed6611
root <mburu.elvis1@students.jkuat.ac.ke>
2023-01-05 09:26:15 +0000

    Docs

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @property
     def width(self):
         """width getter."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -31,7 +31,7 @@ class Rectangle:
     @property
     def height(self):
         """Height Getter."""    
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
