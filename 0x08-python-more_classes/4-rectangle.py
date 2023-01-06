09fe1d06c02981fda4298d507f8207a107456a9b
root <mburu.elvis1@students.jkuat.ac.ke>
2023-01-06 08:38:51 +0000

    pycodestyle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,7 +59,9 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width] * self.height)
+
     def __repr__(self):
         """Return a string representation of the rectangle object."""
-        rect = "Rectangle("+ str(self.__width) + "," + str(self.__height) + ")"
+        rect = "Rectangle("+ str(self.__width) + ","
+        rect = rect + str(self.__height) + ")"
         return rect
