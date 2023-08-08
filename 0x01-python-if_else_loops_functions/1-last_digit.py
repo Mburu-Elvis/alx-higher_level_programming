d0faf500a081b16888ee5a2337061b2af50a022a
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-08 20:37:19 +0000

    1-last_digit.py negative numbers debugging

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     x = number * -1
-    mod = x % 10
+    mod = x % 10 * -1
 else:
     mod = number % 10
 if mod > 5:
@@ -11,5 +11,5 @@ if mod > 5:
 elif mod == 0:
     print('Last digit of', number, 'is', mod, 'and is 0')
 else:
-    print('Last digit of', number, 'is', mod,\
-    'and is less than 6 and 0')
+    print('Last digit of', number, 'is', mod, end='')
+    print(' and is less than 6 and 0')
