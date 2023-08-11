57a71581759ba7520eaf2f61c807c07bd6897a7e
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-11 00:11:30 -0700

    Task 2 requirement check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ from sys import argv
 def display_args():
     x = len(argv) - 1
     if x == 1:
-        print('{:d} argument:\n{:d}: {}'.format(x, x, argv[x]))
+        print('{:d} argument:'.format(x))
+    elif x == 0:
+        print('{:d} arguments.'.format(x))
     else:
         print('{:d} arguments:'.format(x))
     for i in range(1, x + 1):
