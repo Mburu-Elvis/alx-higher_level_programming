00a6bb0bc02e4392d8b146e8c868022d67327959
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-14 14:59:36 -0700

    debug task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list) - 1
-    for x in range(i + 1):
-        print('{:d}'.format(my_list[i]))
-        i -= 1
+    i = len(my_list)
+    new_list = my_list[i - 1::-1]
+    for x in new_list:
+        print('{:d}'.format(x))
