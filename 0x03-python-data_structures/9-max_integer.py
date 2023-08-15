382005a2ff4aa19f85c627334590870ec3dea998
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-15 07:06:24 -0700

    9-max_integr.py script

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,8 @@ def max_integer(my_list=[]):
     if my_list is None:
         return None
     max_num = 0
+    if len(my_list) == 0:
+        return (None)
     for i in range(len(my_list)):
         if my_list[i] > max_num:
             max_num = my_list[i]
