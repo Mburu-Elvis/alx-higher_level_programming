80a4347c5656e3cb6912807284d1092dd8b4f8bc
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-15 02:47:54 -0700

    8-multiple_returns.py script

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,7 +5,7 @@ def multiple_returns(sentence):
     a, b = 0, ''
     if sentence is None:
         b = None
-        a = 0
+        a = None
     else:
         a, b = len(sentence), sentence[0]
     return (a, b)
