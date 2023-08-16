e109e7e1ca060214d053e19804f9a8473dd5f35e
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-16 13:26:26 -0700

    10-best_score.py script

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    best = None
+    if a_dictionary is not None:
+        keys = list(a_dictionary)
+        num = a_dictionary[keys[0]]
+        for key in keys:
+            if a_dictionary[key] > num:
+                best = key
+                num = a_dictionary[key]
+    return (best)
