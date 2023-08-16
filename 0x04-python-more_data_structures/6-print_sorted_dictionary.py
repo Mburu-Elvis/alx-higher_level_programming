f6b2e7fe00960b144a6988c57f31f6365da2a560
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-16 12:12:37 -0700

    6-print_sorted_dictionary.py script

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    '''print a sorted dictionary'''
+    sort_dict = sorted(list(a_dictionary))
+    for i in sort_dict:
+        print("{}: {}\n".format(i, a_dictionary[i]))
