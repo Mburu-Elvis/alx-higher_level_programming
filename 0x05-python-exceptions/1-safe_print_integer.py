23c6798da74b58d09d61f7b13acc1c3619b0ecfd
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-28 15:59:22 -0700

    1-safe_print_integer.py script

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,10 +3,8 @@
 def safe_print_integer(value):
     '''function that prints an integer'''
 
-    num = False
     try:
         print("{:d}".format(value))
-        num = True
+        return True
     except ValueError:
-        num = False
-    return num
+        return False
