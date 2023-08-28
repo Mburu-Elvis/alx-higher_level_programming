4e01cd50177988334e637d3590981956b930d5fd
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-08-28 15:44:44 -0700

    debug task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 def safe_print_list(my_list=[], x=0):
     '''Function that prints x elements of a list'''
 
+    count = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
+            count += 1
         except IndexError:
-            i -= 1
             break
     print()
-    i += 1
-    return (i)
+    return (count)
