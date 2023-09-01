804cb03d3cf09054c39d7b694e413d28704e16a0
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-01 14:12:12 -0700

    1-square.py, 'Square' class with '__init__'

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""A modeule cnataining a Square class."""
+
+
+class Square:
+    """a square definition.
+
+    Provides the attributes for all squares.
+
+    """
+    def __init__(self, size):
+        """constructor to initalialize the object after creation"""
+
+        self.__size = size
