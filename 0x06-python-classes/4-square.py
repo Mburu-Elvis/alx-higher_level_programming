e31ab00a5bb32326942b1feb665b31861c78d263
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-01 15:19:01 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module defining a Square class"""
+
+
+class Square:
+    """Definition of the attributes of a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """__size attribute getter."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """__size attribute setter."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size mut be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the are of the square."""
+        return (self.__size * self.__size)
