33baa3b497a82495f4986de9124b196579e77a83
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-04 14:26:12 -0700

    debugging 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Class definition"""
     def __init__(self, width=0, height=0):
         """the object instatiation function."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
