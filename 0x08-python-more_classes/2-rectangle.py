3505af9d9a531f3ae3d5f4a69d1316ed23833a47
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-04 14:36:12 -0700

    2-rectangle.py, area and perimeter methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,6 @@ class Rectangle:
 
     def perimeter(self):
         """function to calculate the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return (0)
         return (2 * (self.width) + 2 * (self.height))
