f0daa5be272b18680775cd9cbe97f470285ac1a3
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-04 13:59:46 -0700

    1-rectangle.py script

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module defining class Rectangle"""
+
+
+class Rectangle:
+    """Class definition"""
+    def __init__(self, width=0, height=0):
+        """the object instatiation function."""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """the width getter function."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """the width setter function."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height getter function."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """the height of the rectangle setter."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
