9953d8d0b5b6e482fd8dc0a5eac6ecb18f1865f4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-04 14:21:29 -0700

    debugging 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,16 +6,7 @@ class Rectangle:
     """Class definition"""
     def __init__(self, width=0, height=0):
         """the object instatiation function."""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
@@ -26,11 +17,19 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """the width setter function."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
         """The height getter function."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         return self.__height
 
     @height.setter
