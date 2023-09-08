33b54e29f298832a1a24effa5cfa27a58da942de
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-08 15:22:41 -0700

    5-text_indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,11 +4,11 @@
 
 def text_indentation(text):
     """function that prints a text with 2 new lines after '.', '?' and ':'"""
-    if not isinstance(text, str) or text == None:
+    if not isinstance(text, str) or text is None:
         raise TypeError("text must be a string")
     prev = False
     for char in text:
-        if text[0] == " " or (prev == True and char == " "):
+        if text[0] == " " or (prev is True and char == " "):
             prev = False
             continue
         prev = False
