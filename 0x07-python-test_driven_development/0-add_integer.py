9493d0cfe646878c1349044a0b8a39b3237040a6
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-08 02:51:13 -0700

    0-add_integer.py, tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,13 +9,16 @@
 def add_integer(a, b=98):
     """function to add two integers.
 
-    defualt value of b is 98"""
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    if not isinstance(a, int):
-        raise TypeError(f'{a} must be an integer')
-    if not isinstance(b, int):
-        raise TypeError(f'{b} must be an integer')
-    return (a + b)
+    default value of b is 98"""
+    try:
+        if isinstance(a, float):
+            a = int(a)
+        if isinstance(b, float):
+            b = int(b)
+        if not isinstance(a, int):
+            raise TypeError(f'{a} must be an integer')
+        if not isinstance(b, int):
+            raise TypeError(f'{b} must be an integer')
+        return (a + b)
+    except ValueError as e:
+        print(e)
