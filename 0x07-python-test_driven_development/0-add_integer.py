5b78babb218cf03e62ba9d53e74bb9083cadebfc
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-08 05:08:08 -0700

    debugging task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,9 +16,12 @@ def add_integer(a, b=98):
         if isinstance(b, float):
             b = int(b)
         if not isinstance(a, int):
-            raise TypeError(f'{a} must be an integer')
+            raise TypeError('a must be an integer')
         if not isinstance(b, int):
-            raise TypeError(f'{b} must be an integer')
+            raise TypeError('b must be an integer')
         return (a + b)
-    except ValueError as e:
-        print(e)
+    except:
+        if a == None:
+            raise ValueError("a must be an integer")
+        else:
+            raise ValueError("b must be an integer")
