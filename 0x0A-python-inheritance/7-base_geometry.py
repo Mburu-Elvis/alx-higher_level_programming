c3f994ce9f0188549771b55544dd2918e14695d1
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-11 13:57:37 -0700

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module thats contains BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """method to calculate area"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        """method to validates value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
