fce7e246bb893265c3e30e07ad1a99b8f9439a41
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-11 15:49:40 -0700

    100-my_int/py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,11 @@
 
 
 class MyInt(int):
-
+    """class definition"""
     def __eq__(self, other):
+        """inversion of =="""
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """inversion of !="""
         return super().__eq__(other)
