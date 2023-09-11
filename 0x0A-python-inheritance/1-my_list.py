968f6d38b9cbb6a0905bec7ad592382cdf8b1a08
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-11 16:00:09 -0700

    1-my_int.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """A class MyList that inherits from list"""
+    def __init__(self):
+        """object instantiation."""
+        super().__init__()
+
     def print_sorted(self):
         """public the lists a sorted list"""
         print(sorted(self))
