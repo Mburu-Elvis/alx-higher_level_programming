5c270f9e0abda3187665f4836eea03f0345e13ec
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-12 16:06:13 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Function that reads a text file and prints it to stdout."""
-    with open(filename, encoding="utf-8") as my_file:
-        file_read = my_file.read()
-        print(file_read)
+    with open(filename, encoding="UTF8") as my_file:
+        for line in my_file:
+            print(line, end="")
