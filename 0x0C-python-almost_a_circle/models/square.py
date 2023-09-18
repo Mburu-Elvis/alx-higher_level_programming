c6ec4c20775ba5d6f8c8486430c20937992d71e4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-18 13:38:21 -0700

    size setter and getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """class Square constructor."""
         super().__init__(size, size, x, y, id=id)
+        self.size = size
 
     def __str__(self):
         """str method for class Square."""
