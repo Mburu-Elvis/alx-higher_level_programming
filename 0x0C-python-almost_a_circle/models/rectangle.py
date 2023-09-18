f2be3ccfd69079c1a62af1d488c42fcc63d41677
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-18 07:47:14 -0700

    rectangle.py attribute validation

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Module containing class Rectangle."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle that inherits from Base."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor for class Rectangle."""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """width getter."""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """setter to attribute width."""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """height getter."""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """setter to height attribute."""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        """x getter."""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """setter to  x attribute."""
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        """y getter."""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """setter to y attribute."""
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+"""Module to test the Base class"""
+
+
+class TestRectangle(unittest.TestCase):
+    """TestBase class instatiation, uses Base.TestCase as base class"""
+
+
+    def test_initMethod(self):
+        """Method to test the Base class constructor"""
+        rectangle = Rectangle(width=4, height=8, x=1, y=3)
+        self.assertEqual(rectangle.width, 4)
+        self.assertEqual(rectangle.height, 8)
+
+    def test_init_with_no_id(self):
+        """Method to test id"""
+        rectangle1 = Rectangle(4, 8, 1, 3)
+        rectangle2 = Rectangle(4, 8, 1, 3)
+        self.assertNotEqual(rectangle1.id, rectangle2.id)
+
+if __name__ == "__main__":
+    unittest.main()
