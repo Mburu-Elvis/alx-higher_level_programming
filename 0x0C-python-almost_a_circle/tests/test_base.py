40942db86213379dbc5af6e687a5df27689980e0
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-18 17:19:13 -0700

    test for create method of Rectangle class

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,6 +3,7 @@ import unittest
 import sys
 sys.path.append('..')
 from models.base import Base
+from models.rectangle import Rectangle
 """Module to test the Base class"""
 
 
@@ -25,6 +26,19 @@ class TestBase(unittest.TestCase):
         """Method to test nb_objects increment"""
         obj = Base()
         self.assertEqual(obj.id, Base._Base__nb_objects)
+
+    def test_to_json_string(self):
+        """Method to test to_json_string method of class Base."""
+        obj = Base()
+        my_str = [{"a": 1, "b": 2}, {"c": 3, "d": 4}]
+        str1 = obj.to_json_string(my_str)
+        self.assertEqual(my_str, my_str)
+
+    def test_create(self):
+        """method to test create method of class Base"""
+        dict1 =  {"width": 3, "height": 10}
+        rect = Rectangle.create(**dict1)
+        self.assertEqual(rect.width, 3)
 
 if __name__ == "__main__":
     unittest.main()
