7f770e2065a7b4c4d4d4171d5f92b7e5be0fa117
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-09-18 18:16:25 -0700

    from_json_string method of class Base

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -16,3 +16,10 @@ class TestSquare(unittest.TestCase):
         square = Square(size=2, x=3, y=4, id=1)
         square.update(size=4)
         self.assertTrue(square.size, 4)
+
+    def test_to_dictionary(self):
+        """method to test to_dictionary of Square class."""
+        square = Square(size=7, x=4, y=5, id=10)
+        my_dict = square.to_dictionary()
+        dict1 = {"id": 10, "size": 7, "x": 4, "y": 5}
+        self.assertEqual(my_dict, dict1)
