88ee10205e9cf1c3667c927d2d8b6d825c8191aa
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-12-01 15:16:49 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """script fetching a url"""
-import request
+import requests
 
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    response = request.get(url)
+    response = requests.get(url)
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
     print(f"\t- content: {response.text}")
