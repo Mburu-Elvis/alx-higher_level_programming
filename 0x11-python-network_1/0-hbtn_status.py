5c61e2b208d784f60a8c4a24c2b47fc958634816
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-12-01 13:49:22 -0800

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a script that fetches a URL"""
+import urllib.request
+
+
+url = 'https://alx-intranet.hbtn.io/status'
+def fetch_url():
+    """function that fetches a URL"""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        headers = response.read().decode('utf-8')
+    lines = headers.split('\n')
+    for key in lines:
+        print(f'\t{key}')
+
+
+if __name__ == '__main__':
+    fetch_url()
