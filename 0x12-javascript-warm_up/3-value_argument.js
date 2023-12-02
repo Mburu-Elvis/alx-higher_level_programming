ba65ea6dea65c1c75ccc23c386dcc5fb74333c7b
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-12-02 12:34:34 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,12 +3,5 @@ const argv = process.argv;
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  let myStr = [];
-  let k = 0;
-  for (let i = 2; argv[i] !== undefined; i++) {
-    myStr[k] = argv[i];
-    k++;
-  }
-  myStr = myStr.join(' ');
-  console.log(myStr);
+  console.log(argv[2]);
 }
