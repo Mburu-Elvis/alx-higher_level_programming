63721f13be8541d9b2039c7d217dae6bc3e9e969
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2023-12-03 14:22:21 -0800

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,6 @@
 const Square1 = require('./5-square');
 
 class Square extends Square1 {
-  constructor (size) {
-    super(size);
-  }
   charPrint (c = 'X') {
     for (let i = 0; i < this.width; i++) {
       let row = '';
