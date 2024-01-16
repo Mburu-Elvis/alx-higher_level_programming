ff76f7135c9f22a3dc23545e099c0d877ffdb0b1
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2024-01-16 01:41:31 -0800

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all states from the database"""
+import sys
+import MySQLdb
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+db = MySQLdb.connect(user=username, password=password, database=database)
+c = db.cursor()
+c.execute("""SELECT id, name FROM states ORDER BY states.id ASC""")
+rows = c.fetchall()
+
+for row in rows:
+    print(f"({row[0]}, '{row[1]}')")
