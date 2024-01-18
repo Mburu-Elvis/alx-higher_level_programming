3f013d223bd8c902a42decc6e75030fb85ba98c3
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2024-01-18 15:33:30 -0800

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script listing all State objects from hbtn_0e_6_usa database."""
+"""script listing all State objects from hbtn_0e_6_usa database
+
+the module receive 3 arguments that are used for processing"""
 from sys import argv
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -11,7 +13,6 @@ pwd = argv[2]
 db = argv[3]
 engine = create_engine(f"mysql+mysqldb://{username}:{pwd}@localhost:3306/{db}")
 Base.metadata.create_all(engine)
-
 Session = sessionmaker(bind=engine)
 session = Session()
 for instance in session.query(State).order_by(State.id):
