52e71b3e31c34a838ccf6be481f21136e13280df
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2024-01-18 21:38:51 -0800

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 script listing all State objects from hbtn_0e_6_usa database.
 
@@ -8,18 +9,18 @@ Usage:
     ./7-model_state_fetch_all.py <username> <password> <database_name>
 """
 
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-username = argv[1]
-pwd = argv[2]
-db = argv[3]
-engine = create_engine(f"mysql+mysqldb://{username}:{pwd}@localhost:3306/{db}")
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-for instance in session.query(State).order_by(State.id).all():
-    print(f'{instance.id} : {instance.name}')
+    username = argv[1]
+    pwd = argv[2]
+    db = argv[3]
+    engine = create_engine(f"mysql+mysqldb://{username}:{pwd}@localhost:3306/{db}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id).all():
+        print(f'{instance.id} : {instance.name}')
