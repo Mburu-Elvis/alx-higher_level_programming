6c5c779f1d9fc34a5382bdeddc09bf538851ba20
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2024-01-18 21:30:10 -0800

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,6 +7,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+"""Demo doc"""
 
 username = argv[1]
 pwd = argv[2]
